eaeb7a027d4b3ab0f40499b2c269cfae60a94f76
Sumit Sheoran <sumitsheoran642@gmail.com>
2025-12-27 22:17:01 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// function sum(a, b){
-//     s = a+b;
-//     return s;
-// }
+function sum(a, b){
+    s = a+b;
+    return s;
+}
 
-// console.log(sum(2,3));
+console.log(sum(2,3));
 
-// const y = (a, b)=>{
-//     console.log(a+b);
-// }
+const u = (a, b)=>{
+    console.log(a+b);
+}
 
-// y(2,3)
+u(2,3)
 
 function vowels(msg){
     let num = 0
@@ -33,4 +33,79 @@ const y = (msg) => {
     console.log(num);
 }
 y("aeious");
+
+//CALLBACK FUNCTIONS
+let arr = [1,2,3,4,5];
+arr.forEach((y)=>{
+    console.log(y);
+})
+
+
+
+
+let arr1 = [1,2,3,4,5];
+arr.forEach((val)=>{
+    console.log(val*val)
+})
+
+//map callback
+let arr2 = [2,3,4,5,6];
+let newArr = arr2.map((val)=>{
+    return val*val;
+})
+
+console.log(newArr);
+
+//filter callback
+
+let arr3 = [1,2,3,4,5,6,7,8,9];
+let newArr1 = arr3.filter((val)=>{
+    return val%2===0;
+})
+
+console.log(newArr1);
+
+//reduce Callback => It takes many input values and return a single input value(like if we want to find average or sum)
+
+let arr4 = [4,3,6,2,7,9,2];
+let x = arr4.reduce((max, el)=>{
+    if(max>el){
+        return max;
+    }else{
+        return el;
+    }
+})
+
+console.log(x);
+
+
+let marks = [92, 90, 78, 95, 93, 99, 100, 87, 5];
+let newArrr = marks.filter((val)=>{
+    return val>90;
+})
+
+console.log(newArrr);
+
+let size = prompt("Enter the size : ");
+let arrr = [];
+let newA = (size)=>{
+
+    for(let i=0;i<size;i++){
+        arrr.push(i);
+    }
+    return arrr;
+}
+console.log(newA(size));
+
+let uu = arrr.reduce((prev, curr)=>{
+    return prev+curr;
+})
+
+console.log(uu);
+
+let tu = arrr.reduce((prev, curr)=>{
+    return prev*curr;
+})
+
+console.log(tu);
 
